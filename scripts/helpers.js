4700377849ebe5d982315db31d51f1c38e6a3d79
Valeriy <mypowerismind@gmail.com>
2016-01-11 16:02:07 +0200

    Initial commit

---
+++ scripts/helpers.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	SetHeightOfLandTitle();
+});
+
+function SetHeightOfLandTitle() {
+	
+	var headerFlex = $('.lnd.block.content .flex-container.with-last-child-fill');
+
+	for(i=0;i<headerFlex.length;i++){
+		var headerChildren = $(headerFlex[i]).children();
+	
+		var menuHeight = parseInt($(headerChildren[0]).css('height')) + parseInt($(headerChildren[0]).css('margin-top'));
+
+		var headerFlexHeight = parseInt($(headerFlex).css('height'));
+
+		$(headerChildren[1]).css({'height': (headerFlexHeight - menuHeight) + "px"});
+	}
+};
