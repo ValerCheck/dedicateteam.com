e1d30c33e6d93ecef724f0fabae6145c10972973
Valeriy <mypowerismind@gmail.com>
2016-01-20 00:00:31 +0200

    Add menu for mobile view. Used flex.

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,5 +1,49 @@
 $(document).ready(function() {
 	SetHeightOfLandTitle();
+
+	$('#menu-burger').click(function() {
+		var menuFirstPart = $('.menu-li.order-1');
+		var menuSecondPart = $('.menu-li.order-3');
+
+		menuFirstPart.css({'opacity':'1','position':'relative',"top":"0","background-color":"rgba(85,85,85,0.5)"});
+		menuSecondPart.css({'opacity':'1','position':'relative',"top":"0","background-color":"rgba(85,85,85,0.5)"});
+
+		$('#menu-burger').focus();
+
+		if ($('.mobile-menu').length == 0)
+		{
+			var menu = "<div class=\"mobile-menu\"></div>";
+			$('.header-menu').append(menu);
+			$('.mobile-menu').prepend(menuSecondPart);
+			$('.mobile-menu').prepend(menuFirstPart);
+		}
+		else if ($('.mobile-menu').css('opacity') == 1) {
+			$('.mobile-menu').css('opacity','0');
+		} else if ($('.mobile-menu').css('opacity') == 0) {
+			$('.mobile-menu').css('opacity','1');
+		}
+		
+	});
+
+	$('#menu-burger').blur(function() {
+		if ($('.mobile-menu').css('opacity') == 1) {
+			$('.mobile-menu').css('opacity','0');
+		} else if ($('.mobile-menu').css('opacity') == 0) {
+			$('.mobile-menu').css('opacity','1');
+		}
+	});
+
+	/*$('#menu-burger').mouseleave(function(event) {
+		$('.menu-burger:focus').length == 1) {
+			if ($('.mobile-menu').css('opacity') == 1) {
+				$('.mobile-menu').css('opacity','0');
+			} else if ($('.mobile-menu').css('opacity') == 0) {
+				$('.mobile-menu').css('opacity','1');
+			}
+		}
+
+	});*/
+
 });
 
 function SetHeightOfLandTitle() {
