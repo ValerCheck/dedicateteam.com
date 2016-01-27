c4b5dff7187da4fb44f292090bb4e1a7a44c7138
Valeriy <mypowerismind@gmail.com>
2016-01-27 11:38:51 +0200

    Add portfolio page. Add three sliders to index page.

--- scripts/no-flex-helpers.js
+++ scripts/no-flex-helpers.js
@@ -1,28 +1,81 @@
 $(document).ready(function() {
+
+	$('.best-solution .slider').slick({
+		centerMode: true,
+		autoplay: true,
+		centerPadding: 0,
+		autoplaySpeed: 5000,
+		speed: 1000,
+		slidesToShow: 1,
+		responsive: [
+    		{
+	    		breakpoint: 860,
+		      	settings: {
+		        	arrows: true,
+		        	centerMode: true,
+		        	slidesToShow: 1
+		    	}
+		    },
+		    {
+		      	breakpoint: 700,
+		      	settings: {
+		        	arrows: true,
+		        	centerMode: true,
+		        	slidesToShow: 1
+		      	}
+		    }
+	  	]
+	});
+
+	$('.feat-slider').slick({
+		centerMode: true,
+		nextArrow:".feat-slider-btn.right",
+		prevArrow:".feat-slider-btn.left",
+		centerPadding: 0,
+		slidesToShow: 3,
+		responsive: [
+    		{
+	    		breakpoint: 1100,
+		      	settings: {
+		        	arrows: true,
+		        	centerMode: true,
+		        	slidesToShow: 2
+		    	}
+		    },
+		    {
+		      	breakpoint: 800,
+		      	settings: {
+		        	arrows: true,
+		        	centerMode: true,
+		        	slidesToShow: 1
+		      	}
+		    }
+	  	]
+	});
+
 	$('.partners-items').slick({
 		centerMode: true,
 		autoplay: true,
 		autoplaySpeed: 5000,
+		centerPadding: 0,
 		slidesToShow: 3,
 		responsive: [
-    	{
-	      breakpoint: 860,
-	      settings: {
-	        arrows: true,
-	        centerMode: true,
-	        centerPadding: '40px',
-	        slidesToShow: 2
-	      }
-	    },
-	    {
-	      breakpoint: 700,
-	      settings: {
-	        arrows: true,
-	        centerMode: true,
-	        centerPadding: '40px',
-	        slidesToShow: 1
-	      }
-	    }
-	  ]
+    		{
+	    		breakpoint: 860,
+		      	settings: {
+		        	arrows: true,
+		        	centerMode: true,
+		        	slidesToShow: 2
+		    	}
+		    },
+		    {
+		      	breakpoint: 700,
+		      	settings: {
+		        	arrows: true,
+		        	centerMode: true,
+		        	slidesToShow: 1
+		      	}
+		    }
+	  	]
 	});
 });
